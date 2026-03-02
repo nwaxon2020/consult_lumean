4322e20d958a955244e47eddfa4d42c5c33fb2b9
Prince Ogbonnaya Nwachukwu <131527050+nwaxon2020@users.noreply.github.com>
2026-03-02 12:07:00 +0100

    Add Admin component with creation message

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function Admin(){
+  return(
+    <div className='mx-auto p-24 bg-black text-white text-center'>
+      <p>Admin Creation Coming soon</p>
+      <small>Here you get to manage your site dashboard</small>
+    </div>
+  )
+}
